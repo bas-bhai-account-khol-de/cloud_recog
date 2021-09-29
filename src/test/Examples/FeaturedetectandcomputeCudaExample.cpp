6d7cfd5e1b19bcc3c2684b50533171c566e42f7a
bas-bhai-account-khol-de <prakhar.srivastava@flamapp.com>
2021-09-30 01:38:00 +0530

    matched features

--- src/test/Examples/FeaturedetectandcomputeCudaExample.cpp
+++ src/test/Examples/FeaturedetectandcomputeCudaExample.cpp
@@ -1,1 +1,17 @@
 
+    dbg_messsage("Copmutation started")
+    vision::cuda::Stream current_stream;
+    vision::cuda::GpuMat gpuimage;
+    dbg_messsage("ran until decleration")
+    vision::Mat image = vision::imread("/media/prakhar/Linux FIles/Flam Apps/Testing and experiment/DataSets/school_bag/Image_1 Bag.jpg",vision::IMREAD_GRAYSCALE);
+    vision::imshow("frame",image);
+    vision::waitKey(0);
+    vision::Mat temp;
+    dbg_messsage("ran until decleration")
+    gpuimage.upload(image);
+    vision::Ptr<vision::cuda::ORB> p_orb_d = vision::cuda::ORB::create();
+    p_orb_d->setBlurForDescriptor(true);
+    vision::cuda::GpuMat kp_d;
+    vision::cuda::GpuMat desc_d;
+    p_orb_d->detectAndComputeAsync(gpuimage,cv::cuda::GpuMat(), kp_d, desc_d, false, current_stream);
+    dbg_messsage("Feature points detected and computed")
